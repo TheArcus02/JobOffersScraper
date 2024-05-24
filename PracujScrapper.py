6bccdad6512b7917ac5289b7e5001dbff5e04e63
TheArcus02 <m.urbaniak.inf@gmail.com>
2024-05-24 20:58:24 +0200

    Minor convention changes

--- PracujScrapper.py
+++ PracujScrapper.py
@@ -7,9 +7,9 @@ import time
 import requests
 from dotenv import load_dotenv
 
+from Database import Database
+from EmailSender import EmailSender
 from constants import LOGIN_URL, USER_AGENT, JSON_PATTERN, OFFERS_URL, OFFERS_HTML_TEMPLATE
-from database import Database
-from emailSender import EmailSender
 from utils import save_to_file
 
 
@@ -25,6 +25,19 @@ class PracujScrapper:
             'accept': '*/*',
             'accept-language': 'pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7',
             'content-type': 'application/json; charset=utf-8',
+            'cookie': 'gp__cfXfiDZP=78; __cf_bm=JfSolsKxM.2fZpLc4qiJCycNVi6TC3VjHRQRbZLfz9s-1716204820-1.0.1.1-IuXz4_snWY5brRDnMKmeh6KhmkJgzmSm0edOuzNtzTIh.xePb2kf_nZDMKwC8SsBYGvhAaena3CyPNq0eXkWvg; __cfruid=829b5f8620d1324606978f1745f1eb8875bd88eb-1716204820; _cfuvid=3c.0CHBMKZZJw25QaZlQnC2CxbsJEN67DZBqCnhu__w-1716204820272-0.0.1.1-604800000; gptrackCookie=3ee511fe-d63d-416a-ye1a-07b5ef4d0b37; gp_lang=pl; cf_clearance=EDLXiQ.C0fsUSOU_4pXJ4ktYAG..vGrVXCBZGGf5lWQ-1716204821-1.0.1.1-lSjPN9bVWYfDunsTKRW0eVfRW1h2Dpdp00eKsU9QVI4ijSE2I7CwEzHtNQy5I9ogdsRs0i6c.se7k89BNoNWTQ; _gpantiforgery=CfDJ8I6hLzPzDNhHkh1OJJZeYhMNJkHtTme_4Sewxa3pZ86uzv_VMH3IThH1uQ3lB6wB0UoQuYa_adn0-9sJROMN23Wh9coxWA0YKp0RIJ3LMmwAUr8SYw6y95HIU0AyRupWD3zCVOKeWysY-uVzAnp-jYg; gpc_v=1; gpc_analytic=1; gpc_func=1; gpc_ad=1; gpc_audience=1; gpc_social=1; gp_ab__sales__205=B; _gpauthclient=pracuj.pl; XSRF-TOKEN=CfDJ8I6hLzPzDNhHkh1OJJZeYhMpEYSrTiiXQRn4OtNzz4fBkEPmcIk5_IYw-7YHCKVR_Vu9aqjuO_K7C3KN-oZC_rqNtdn7iE3XYgRrgA7a5QrQduzA_IGu6kaHOLvuqH_0247OA7GTPlmPJmigrhj6_Kw; gptrackPVID=f9e51301-bb81-4455-ae18-7214c0fa76cd; x-auth-csrf-token=cb05b2b1fd016dcb4644443a2b6e626eb902aba336a9a29fd2651a6e56abbc16; gptrackPVID=d7ece9a1-ab24-47cd-y81f-2c8edfdb78af',
+            'origin': 'https://login.pracuj.pl',
+            'priority': 'u=1, i',
+            'referer': 'https://login.pracuj.pl/logowanie?aupid=73cf0224-ee40-485a-a875-41adb086d6e7',
+            'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': USER_AGENT,
+            'x-auth-csrf-hash': '56be1a47db0f21fe7d4cc02f26b5b2c496e15f7a852abaa9ac3b891f24399e4a',
+            'x-kl-ajax-request': 'Ajax_Request',
             'User-Agent': USER_AGENT
         }
         data = {
@@ -65,7 +78,7 @@ class PracujScrapper:
             print(f'No matches found...')
             return None
 
-    def validate_and_save_offers(self, offers):
+    def validate_offers(self, offers):
         new_offers = []
         for offer in offers:
             if not self.db.offer_exists_from_json(offer):
@@ -87,14 +100,13 @@ class PracujScrapper:
                 # Wait before next request to prevent ban
                 time.sleep(5)
 
-        # If new offers were found send an email.
-        if new_offers:
-            current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            subject = f'New Job Offers - {current_time}'
-            body = self.build_email_body(new_offers)
-            self.email_sender.send_email(subject, body)
-        else:
-            print('No new offers found.')
+        return new_offers
+
+    def send_email_with_offers(self, offers):
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        subject = f'New Job Offers - {current_time}'
+        body = self.build_email_body(offers)
+        self.email_sender.send_email(subject, body)
 
     def get_offers(self):
         html = self.scrape_page(OFFERS_URL)
--- main.py
+++ main.py
@@ -8,28 +8,17 @@ def scrapePracuj():
     token = scrapper.login()
     print('scrapping offers...')
     offers_json = scrapper.get_offers()
+    print(f'Found {len(offers_json)} offers.')
     print('validating offers...')
-    scrapper.validate_and_save_offers(offers_json)
-
-
-def testing_scrape():
-    pass
-    # scrapper = PracujScrapper()
-    #
-    # with open('data/offers_page.html', 'r', encoding='utf-8') as offers_file:
-    #     offers_html = offers_file.read()
-    #     offers_json = scrapper.get_json_from_html(offers_html)
-    #     # print(offers_json)
-    #     with open('data/offer_details.html', 'r', encoding='utf-8') as details_file:
-    #         offer_details_html = details_file.read()
-    #         full_offer_details_json = scrapper.get_json_from_html(offer_details_html)
-    #         print(json.dumps(full_offer_details_json, indent=4),
-    #               open('data/full_offer_details.json', 'w', encoding='utf-8'))
-    #         offer_details = scrapper.extract_offer_details(full_offer_details_json)
-    #         print(offer_details)
+    validated_offers = scrapper.validate_offers(offers_json)
+    if not validated_offers:
+        print('No new offers found.')
+        return
+    print(f'Validated {len(validated_offers)} offers.')
+    print('sending email...')
+    scrapper.send_email_with_offers(validated_offers)
 
 
 if __name__ == '__main__':
     pass
     scrapePracuj()
-    # testing_scrape()
