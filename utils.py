d875be6a6b1f4d2904975c0248005ea9ae5488d6
TheArcus02 <m.urbaniak.inf@gmail.com>
2024-05-24 21:40:19 +0200

    Fix file saving issue by ensuring 'data' directory exists

--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+import os
+
+
 def save_to_file(data, filename, encoding=None):
+    os.makedirs('data', exist_ok=True)
     with open(f'data/{filename}', 'w', encoding=encoding) as file:
         file.write(data)
